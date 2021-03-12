c3d6fbbb7758bc80edfe4d56a9d81f3da61f4126
Klebson Mendes <kelmendes>
2021-03-12 10:05:09 -0300

    Organização Model, PageObjects

---
+++ src/main/java/models/SauceDemoLoginMO.java
@@ -0,0 +1,44 @@
+package models;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import pageobjects.SauceDemoCartPO;
+import pageobjects.SauceDemoLoginPO;
+import stepdefinition.hook.Hook;
+
+public class SauceDemoLoginMO {
+    WebDriver driver;
+    SauceDemoLoginPO objectLogin = new SauceDemoLoginPO();
+    SauceDemoCartPO objectCart = new SauceDemoCartPO();
+
+    public SauceDemoLoginMO() {
+        this.driver = Hook.getsDrivers();
+    }
+
+    public void abrirBrowser(String urlSite){
+        this.driver.get(urlSite);
+    }
+
+    public void fecharBrowser() {
+//        this.driver.close();
+        System.out.println("Browser Fechado!");
+    }
+
+
+    public void login (String nome, String senha){
+        driver.findElement(By.xpath(objectLogin.inputUserName)).sendKeys(nome);
+        driver.findElement(By.xpath(objectLogin.inputPassword)).sendKeys(senha);
+    }
+
+    public void clickBtnLogin() {
+        driver.findElement(By.xpath(objectLogin.btnLogin)).click();
+    }
+
+    public void validarLoginHome() {
+        driver.findElement(By.xpath(objectCart.divContainerListaDeItensHome)).isDisplayed();
+    }
+
+    public void alertaUsuarioBloqueadoOUUsuarioOuSenhaErrado() {
+        driver.findElement(By.xpath(objectLogin.alertErroLogin)).isDisplayed();
+    }
+}
---
+++ src/main/java/pageobjects/SauceDemoLoginPO.java
@@ -0,0 +1,10 @@
+package pageobjects;
+
+public class SauceDemoLoginPO {
+
+    public String inputUserName = "//*[@id=\'user-name\']";
+    public String inputPassword = "//*[@id=\'password\']";
+    public String btnLogin = "//*[@id=\'login-button\']";
+    public String alertErroLogin = "//h3[@data-test='error']";
+
+}
