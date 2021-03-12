ae681ef0112d0f368414e57b5dd1f570f00ccaa8
Klebson Mendes <kelmendes>
2021-03-12 10:19:36 -0300

    Mudança Fechar navegador, alterado para HOOK e step foi alterada para fazer logout

--- src/main/java/pageobjects/SauceDemoLoginPO.java
+++ src/main/java/pageobjects/SauceDemoLoginPO.java
@@ -6,5 +6,7 @@ public class SauceDemoLoginPO {
     public String inputPassword = "//*[@id=\'password\']";
     public String btnLogin = "//*[@id=\'login-button\']";
     public String alertErroLogin = "//h3[@data-test='error']";
+    public String btnMenuLateral = "//*[@id=\"react-burger-menu-btn\"]";
+    public String btnLogoutMenuLateral = "//*[@id=\"logout_sidebar_link\"]";
 
 }
