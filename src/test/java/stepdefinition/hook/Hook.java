54c8cb5c17f83abe5c602515af8bd7bd9ce0ad48
Klebson Mendes <klerbersonmendes@gmail.com>
2022-03-01 14:35:23 -0300

    Adicionando o takeScreenShot

--- src/test/java/stepdefinition/hook/Hook.java
+++ src/test/java/stepdefinition/hook/Hook.java
@@ -3,7 +3,11 @@ package stepdefinition.hook;
 import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -15,7 +19,7 @@ public class Hook {
     public static WebDriver driver;
 
     @Before
-    public void tearUP(){
+    public void tearUP(Scenario scenario){
 
         System.out.println("+++++++++ UP - HOOK +++++++++++");
 
@@ -26,15 +30,19 @@ public class Hook {
 
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-
+        driver.manage().window().setSize(new Dimension(1024, 542));
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
+
+        System.out.println("Before Scenario: " + scenario.getName());
 
     }
 
     @AfterStep
-    public void setScreenShot(){
-
+    public void setScreenShot(Scenario scenario) throws NoSuchFieldException, IllegalAccessException {
+        System.out.println("Append Screen Shot, After Step!");
+        byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot, "image/png", "name");
     }
 
 
@@ -43,7 +51,8 @@ public class Hook {
     }
 
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario){
+        System.out.println("After Scenario: " + scenario.getName());
         System.out.println("+++++++++ Down - HOOK +++++++++++");
         driver.close();
     }
